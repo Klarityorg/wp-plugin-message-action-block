74eb44159cbb77a41a979c5aff93fa87b0aa094a
Fredrik Areschoug <fredrik@klarity.org>
2019-03-01 14:20:11 +0100

    message action plugin follows naming convention

--- src/block/block.js
+++ src/block/block.js
@@ -19,7 +19,7 @@ const iconEl = el('svg', { width: 128, height: 128, viewBox: "0 0 128 128" },
     el('path', { d: "M41.7607 39.0615H52.8432V60.866L73.2637 39.0615H86.6547L66.1434 60.2237L87.5885 88.9388H74.2753L58.66 67.706L52.8432 73.6982V88.9388H41.7607V39.0615Z", fill: "white" })
 );
 
-registerBlockType( 'klarity/message-action', {
+registerBlockType( 'klarity/klarity-message-action-block', {
   title: __( 'Message action' ),
   icon: iconEl,
   category: 'common',
